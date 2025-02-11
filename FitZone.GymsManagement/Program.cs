232db0e5927dc3c18813b5ab660216efdc071e1f
Cosminmoisa97 <Moisa.IO.Cosmin@student.utcluj.ro>
2025-02-11 20:29:57 +0200

    Add connection string/ program dependency

--- FitZone.GymsManagement/Program.cs
+++ FitZone.GymsManagement/Program.cs
@@ -1,3 +1,6 @@
+using FitZone.GymsManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContextPool<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
