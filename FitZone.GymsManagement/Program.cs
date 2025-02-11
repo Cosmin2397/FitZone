86dd2cb01a0880a003b1a5c11aae0bd2c478f373
Cosminmoisa97 <Moisa.IO.Cosmin@student.utcluj.ro>
2025-02-11 20:32:32 +0200

    Add dependency injection for repositories and services

--- FitZone.GymsManagement/Program.cs
+++ FitZone.GymsManagement/Program.cs
@@ -1,4 +1,8 @@
 using FitZone.GymsManagement.Data;
+using FitZone.GymsManagement.Repositories;
+using FitZone.GymsManagement.Repositories.Interfaces;
+using FitZone.GymsManagement.Services;
+using FitZone.GymsManagement.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IGymRepository, GymRepository>();
+builder.Services.AddScoped<IWorkingHoursRepository, WorkingHoursRepository>();
+builder.Services.AddScoped<IGymService, GymService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
