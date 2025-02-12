c39e75bde74f1a670d1821de078c57209fe3ccab
Cosminmoisa97 <Moisa.IO.Cosmin@student.utcluj.ro>
2025-02-12 19:00:39 +0200

    Add working hours interface methos and implementation in repository

--- FitZone.GymsManagement/Repositories/Interfaces/IWorkingHoursRepository.cs
+++ FitZone.GymsManagement/Repositories/Interfaces/IWorkingHoursRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace FitZone.GymsManagement.Repositories.Interfaces
+﻿using FitZone.GymsManagement.Entities;
+
+namespace FitZone.GymsManagement.Repositories.Interfaces
 {
     public interface IWorkingHoursRepository
     {
+        Task<BusinessHours> AddWorkingHours(BusinessHours workingHours);
+
+        Task<BusinessHours> RemoveWorkingHours(Guid id);
+
+        Task<BusinessHours> UpdateWorkingHours(BusinessHours gym, Guid id);
+
+        Task<BusinessHours> GetWorkingHoursByDay(DayOfWeek day);
+
+        Task<List<BusinessHours>> GetGymWorkingHours(Guid gymId);
     }
 }
--- FitZone.GymsManagement/Repositories/WorkingHoursRepository.cs
+++ FitZone.GymsManagement/Repositories/WorkingHoursRepository.cs
@@ -1,8 +1,122 @@
-﻿using FitZone.GymsManagement.Repositories.Interfaces;
+﻿using FitZone.GymsManagement.Data;
+using FitZone.GymsManagement.Entities;
+using FitZone.GymsManagement.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitZone.GymsManagement.Repositories
 {
     public class WorkingHoursRepository :  IWorkingHoursRepository
     {
+        public readonly AppDbContext _context;
+        public WorkingHoursRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<BusinessHours> AddWorkingHours(BusinessHours businessHours)
+        {
+            if (businessHours == null)
+            {
+                return null;
+            }
+            else
+            {
+                try
+                {
+                    await _context.GymBusinessHours.AddAsync(businessHours);
+                    await _context.SaveChangesAsync();
+
+                }
+                catch (DbUpdateException ex)
+                {
+                    Console.WriteLine($"Eroare la salvarea datelor în baza de date: {ex.Message}");
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Eroare neașteptată: {ex.Message}");
+                    throw;
+                }
+            }
+            return businessHours;
+        }
+
+        public async Task<BusinessHours> GetWorkingHoursByDay(DayOfWeek day)
+        {
+            try
+            {
+                return await _context.GymBusinessHours.FirstOrDefaultAsync(c => c.DayOfWeek == day);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Eroare în timpul extragerii datelor: {ex.Message}");
+                throw;
+            }
+        }
+
+
+        public async Task<List<BusinessHours>> GetGymWorkingHours(Guid gymId)
+        {
+            try
+            {
+                return await _context.GymBusinessHours.Where(z => z.GymId == gymId).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Eroare în timpul extragerii datelor: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<BusinessHours> RemoveWorkingHours(Guid id)
+        {
+            var dayBusinessHours = await _context.GymBusinessHours.FirstOrDefaultAsync(a => a.Id == id);
+            try
+            {
+                if (dayBusinessHours != null)
+                {
+                    _context.GymBusinessHours.Remove(dayBusinessHours);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Eroare neașteptată: {ex.Message}");
+                throw;
+            }
+            return dayBusinessHours;
+        }
+
+        public async Task<BusinessHours> UpdateWorkingHours(BusinessHours businessHours, Guid id)
+        {
+            var dbBusinessHours = await _context.GymBusinessHours.FirstOrDefaultAsync(i => i.Id == id);
+            if (dbBusinessHours != null && businessHours != null)
+            {
+                dbBusinessHours.DayOfWeek = businessHours.DayOfWeek;
+                dbBusinessHours.OpeningHour = businessHours.OpeningHour;
+                dbBusinessHours.ClosingTime = businessHours.ClosingTime;
+                dbBusinessHours.Comment = businessHours.Comment;
+                dbBusinessHours.GymId = businessHours.GymId;
+                dbBusinessHours.IsClosed = businessHours.IsClosed;
+            }
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BusinessHoursExists(id))
+                {
+                    throw;
+                }
+            }
+
+            return businessHours;
+        }
+
+        private bool BusinessHoursExists(Guid id)
+        {
+            return _context.GymBusinessHours.Any(e => e.Id == id);
+        }
     }
 }
