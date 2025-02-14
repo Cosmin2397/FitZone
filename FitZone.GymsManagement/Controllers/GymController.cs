b2ae32718676784ae1c73eecc1af2fd259881a7c
Cosminmoisa97 <Moisa.IO.Cosmin@student.utcluj.ro>
2025-02-14 17:07:15 +0200

    Add gym controller and methods for GET/POST/PUT/DELETE

--- FitZone.GymsManagement/Controllers/GymController.cs
+++ FitZone.GymsManagement/Controllers/GymController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FitZone.GymsManagement.Dtos;
+using FitZone.GymsManagement.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FitZone.GymsManagement.Controllers
@@ -7,5 +10,77 @@ namespace FitZone.GymsManagement.Controllers
     [ApiController]
     public class GymController : ControllerBase
     {
+        private readonly IGymService gymService;
+
+        public GymController(IGymService gymService)
+        {
+            this.gymService = gymService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GymDTO>>> GetGyms()
+        {
+            return await gymService.GetGyms();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GymDTO>> GetGymById(Guid id)
+        {
+            var gym = await gymService.GetGymById(id);
+
+            if (gym == null)
+            {
+                return NotFound();
+            }
+
+            return gym;
+        }
+
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGym(Guid id, GymDTO gymUpdated)
+        {
+            GymDTO updatedGym;
+            try
+            {
+                 updatedGym = await gymService.UpdateGym(gymUpdated, id);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(updatedGym);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GymDTO>> PostGym(GymDTO gym)
+        {
+            try
+            {
+                return await gymService.AddGym(gym);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> DeleteGym(Guid id)
+        {
+            try
+            {
+                return await gymService.RemoveGym(id);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
     }
 }
+
